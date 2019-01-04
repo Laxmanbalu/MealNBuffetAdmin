016c82e78fc04c2e8201ea88672a6d0141c4bbf1
Laxman Dodda <vc.laxman.dodda@lowes.com>
2019-01-04 15:29:52 +0530

    showing food items list

---
+++ app/src/main/java/admin/mealbuffet/com/mealnbuffetadmin/nav/FoodItemsAdapter.kt
@@ -0,0 +1,59 @@
+package admin.mealbuffet.com.mealnbuffetadmin.nav
+
+import admin.mealbuffet.com.mealnbuffetadmin.R
+import admin.mealbuffet.com.mealnbuffetadmin.model.FoodItem
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.mealbuffet.controller.ActionListener
+import kotlinx.android.synthetic.main.fooditem_view.view.*
+
+
+class FoodItemsAdapter(private val requireContext: Context, private val wrapActionListener: ActionListener) : RecyclerView.Adapter<FoodItemsAdapter.FoodItemViewHolder>() {
+
+    private var foodItemsLst: ArrayList<FoodItem>? = null
+
+    fun setData(arrayList: ArrayList<FoodItem>) {
+        this.foodItemsLst = arrayList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodItemViewHolder {
+        val view = LayoutInflater.from(requireContext).inflate(R.layout.fooditem_view, parent, false)
+        return FoodItemViewHolder(view, wrapActionListener, requireContext)
+    }
+
+    override fun getItemCount(): Int {
+        return foodItemsLst?.size ?: 0
+    }
+
+    override fun onBindViewHolder(foodItemViewHolder: FoodItemViewHolder, position: Int) {
+        foodItemsLst?.get(position)?.let { foodItemViewHolder.setData(it) }
+        updateFoodItemImage(foodItemViewHolder, position)
+    }
+
+    private fun updateFoodItemImage(foodItemViewHolder: FoodItemViewHolder, position: Int) {
+        Glide.with(requireContext)
+                .load(foodItemsLst?.get(position)?.image)
+                .into(foodItemViewHolder.itemView.food_item_icon)
+    }
+
+    class FoodItemViewHolder(itemView: View, wrapActionListener: ActionListener, private val requireContext: Context) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener {
+                //                wrapActionListener.onAction(SearchFragment.ACTION_SHOW_RESTAURANT, (itemView.tag as Restaurant))
+            }
+        }
+
+        fun setData(foodItem: FoodItem) {
+            itemView.tag = foodItem
+            itemView.food_items_desc.text = foodItem.desc
+            itemView.food_item_name.text = foodItem.item
+            itemView.food_items_category.text = foodItem.categoryId
+            itemView.food_items_price.text = String.format(requireContext.getString(R.string.item_price), foodItem.price)
+        }
+    }
+}
--- app/src/main/java/admin/mealbuffet/com/mealnbuffetadmin/nav/ItemsListFragment.kt
+++ app/src/main/java/admin/mealbuffet/com/mealnbuffetadmin/nav/ItemsListFragment.kt
@@ -1,17 +1,22 @@
 package admin.mealbuffet.com.mealnbuffetadmin.nav
 
 import admin.mealbuffet.com.mealnbuffetadmin.R
+import admin.mealbuffet.com.mealnbuffetadmin.model.FoodItem
 import admin.mealbuffet.com.mealnbuffetadmin.util.PreferencesHelper
 import admin.mealbuffet.com.mealnbuffetadmin.viewmodel.FoodItemListViewModel
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.mealbuffet.controller.BaseFragment
+import kotlinx.android.synthetic.main.fragment_itemslist.*
 
 class ItemsListFragment : BaseFragment() {
     override fun layoutResource(): Int = R.layout.fragment_itemslist
     private lateinit var foodItemListViewModel: FoodItemListViewModel
+    private lateinit var foodItemsAdapter: FoodItemsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,10 +26,24 @@ class ItemsListFragment : BaseFragment() {
     private fun initFoodItemsListViewModel() {
         foodItemListViewModel = ViewModelProviders.of(this).get(FoodItemListViewModel::class.java)
         foodItemListViewModel.liveData.observe(this, Observer {
-            hideProgress()
+            if (it == null) {
+                showNetworkError()
+            } else {
+                renderFoodItemsView(it)
+            }
         })
-        showProgress()
         val restaurantId = PreferencesHelper.getRestaurantId(requireContext())
         foodItemListViewModel.getFoodItemsData(restaurantId)
+    }
+
+    private fun renderFoodItemsView(foodItemsLst: ArrayList<FoodItem>) {
+        foodItemsAdapter = FoodItemsAdapter(requireContext(), wrapActionListener())
+        foodItems_recyclerView.apply {
+            adapter = foodItemsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        foodItems_recyclerView.itemAnimator = DefaultItemAnimator()
+        foodItemsAdapter.setData(foodItemsLst)
+        foodItemsAdapter.notifyDataSetChanged()
     }
 }
