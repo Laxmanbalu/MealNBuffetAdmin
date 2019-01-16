a46f33800ab030ac99ca25a5d5cbb15900397cfb
Laxman Dodda <laxmanbalu@gmail.com>
2019-01-16 09:35:29 +0530

    added error mechanisam

--- app/src/main/java/admin/mealbuffet/com/mealnbuffetadmin/nav/buffet/AddBuffetFragment.kt
+++ app/src/main/java/admin/mealbuffet/com/mealnbuffetadmin/nav/buffet/AddBuffetFragment.kt
@@ -1,6 +1,7 @@
 package admin.mealbuffet.com.mealnbuffetadmin.nav.buffet
 
 import admin.mealbuffet.com.mealnbuffetadmin.R
+import admin.mealbuffet.com.mealnbuffetadmin.custom.InfoDialog
 import admin.mealbuffet.com.mealnbuffetadmin.model.BuffetBasicData
 import admin.mealbuffet.com.mealnbuffetadmin.util.PreferencesHelper
 import android.app.TimePickerDialog
@@ -9,6 +10,7 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
+import android.widget.EditText
 import com.mealbuffet.controller.BaseFragment
 import kotlinx.android.synthetic.main.fragment_addbuffet.*
 
@@ -25,17 +27,42 @@ class AddBuffetFragment : BaseFragment() {
         et_buffetendTime.inputType = InputType.TYPE_NULL
         et_buffetendTime.setOnClickListener { showEndTimePickerDialog() }
         buffet_movenext.setOnClickListener {
-
-            wrapActionListener().onAction(BUFFET_MOVE_NEXT, makeBuffetBasicObject())
+            val data = makeBuffetBasicObject()
+            if (data != null) {
+                wrapActionListener().onAction(BUFFET_MOVE_NEXT, data)
+            }
         }
     }
 
-    private fun makeBuffetBasicObject(): BuffetBasicData {
+    private fun makeBuffetBasicObject(): BuffetBasicData? {
+        if (showDataError(et_buffetname) || showDataError((et_buffetdisplayname)) || showDataError(et_buffetdesc) || showDataError((et_buffetstartTime)) ||
+                showDataError((et_buffetstartTime)) || showDataError((et_buffetendTime)) || showDataError((et_buffetadult_price)) || showDataError((et_buffetkids_price))) {
+            return null
+        }
+
+        if (spin_buffettype.selectedItem.toString().equals(getString(R.string.default_selection_msg))) {
+            showFoodSelectionError("Select Buffet Type")
+            return null
+        }
         val restaurantId = PreferencesHelper.getRestaurantId(requireContext())
-        var buffetBasic = BuffetBasicData(buffetName = et_buffetname.text.toString(), desc = et_buffetdesc.text.toString(), type = spin_buffettype.selectedItem.toString(),
+        return BuffetBasicData(buffetName = et_buffetname.text.toString(), desc = et_buffetdesc.text.toString(), type = spin_buffettype.selectedItem.toString(),
                 startTime = et_buffetstartTime.text.toString(), endTime = et_buffetendTime.text.toString(), adultPrice = et_buffetadult_price.text.toString().toDouble(),
                 kidsPrice = et_buffetkids_price.text.toString().toDouble(), displayName = et_buffetdisplayname.text.toString(), restaurantId = restaurantId)
-        return buffetBasic
+    }
+
+    private fun showFoodSelectionError(message: String) {
+        val dialog = InfoDialog.newInstance(message)
+        dialog.show(activity?.supportFragmentManager, message)
+    }
+
+    private fun showDataError(etData: EditText): Boolean {
+        return if (etData.text.isEmpty()) {
+            etData.error = getString(R.string.empty_field)
+            true
+        } else {
+            etData.error = null
+            false
+        }
     }
 
     private fun showStartTimePickerDialog() {
